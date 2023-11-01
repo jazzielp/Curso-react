96445cb5a6690cdd5e01e98c0d6409716984cf1c
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-11-01 10:56:15 -0700

    Evento onKeyDown

--- src/StateComponent.jsx
+++ src/StateComponent.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 function StateComponent() {
     const [count, setCount] = React.useState(0)
+    const [myName, setMyName] = React.useState('Nombre')
+
     return (
         <div>
-        <h1>State Component</h1>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
-        <button onClick={() => setCount(0)}>Reset</button>
+            <h1>State Component</h1>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <p>My Name is {myName}</p>
+            <input type="text" onKeyDown={e => { setMyName(e.target.value) }} />
         </div>
     )
 }
